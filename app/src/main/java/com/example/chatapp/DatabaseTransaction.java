f48f1acafca64f4ecd8cdf5c2d68768a125ce8ca
Sagar Vrajalal <sagar.vrajalal@unifize.com>
2021-11-14 00:48:59 +0530

    Update dependencies

--- app/src/main/java/com/example/chatapp/DatabaseTransaction.java
+++ app/src/main/java/com/example/chatapp/DatabaseTransaction.java
@@ -1,8 +1,8 @@
 package com.example.chatapp;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.ListView;
 
 import com.example.chatapp.chats.Chats;
